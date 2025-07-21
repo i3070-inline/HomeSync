74a0560ff68cae1ff9d1d9a71d66d473062c7de1
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2025-07-21 12:14:50 +0300

    update mobile login form: set container width to 80dvw, center form; adjust page padding for improved layout

--- electron.js
+++ electron.js
@@ -2,26 +2,27 @@ const {app, BrowserWindow} = require('electron');
 const path = require('path');
 let mainWindow;
 async function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      contextIsolation: false,
-      nodeIntegration: true,
-    },
-  });
-  await mainWindow.loadURL('https://home-sync-xi.vercel.app');
-  //await mainWindow.loadFile(path.join(__dirname, 'dist', 'smart-house-application', 'browser',
-  // 'index.html'));//browser
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		minWidth: 400,
+		webPreferences: {
+			contextIsolation: false,
+			nodeIntegration: true,
+		},
+	});
+	await mainWindow.loadURL('https://home-sync-xi.vercel.app');
+	//await mainWindow.loadFile(path.join(__dirname, 'dist', 'smart-house-application', 'browser',
+	// 'index.html'));//browser
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
 }
 app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+	if (process.platform !== 'darwin') app.quit();
 });
 app.on('activate', async () => {
-  if (BrowserWindow.getAllWindows().length === 0) await createWindow();
+	if (BrowserWindow.getAllWindows().length === 0) await createWindow();
 });
 
