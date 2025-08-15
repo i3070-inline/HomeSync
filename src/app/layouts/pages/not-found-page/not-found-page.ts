92592bbd41d308c0b694159adb5f6d79336aed19
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2025-08-15 23:32:06 +0300

    feat: update not found page titles for improved clarity and localization

--- src/app/layouts/pages/not-found-page/not-found-page.ts
+++ src/app/layouts/pages/not-found-page/not-found-page.ts
@@ -1,12 +1,14 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from "@angular/core";
 import {RouterLink} from "@angular/router";
 import {TranslatePipe} from "@ngx-translate/core";
+import {UpperCasePipe} from "@angular/common";
 
 @Component({
 	selector: "app-not-found-page",
 	imports: [
 		RouterLink,
-		TranslatePipe
+		TranslatePipe,
+		UpperCasePipe
 	],
 	templateUrl: "./not-found-page.html",
 	styleUrl: "./not-found-page.scss",
