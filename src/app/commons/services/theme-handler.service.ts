e4067845b326d9ee69131fec38460914b36ba700
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2025-08-16 17:38:08 +0300

    feat: update animation attribute to 'none' for improved user experience

--- src/app/commons/services/theme-handler.service.ts
+++ src/app/commons/services/theme-handler.service.ts
@@ -42,10 +42,9 @@ export class ThemeHandlerService extends SettingsHandlerBase<themeType> {
 		this.platformService.runOnBrowserPlatform(async () => {
 			const animationKey = "animation";
 			const before = document.documentElement.getAttribute(animationKey);
-			document.documentElement.setAttribute(animationKey, "reduce");
+			document.documentElement.setAttribute(animationKey, "none");
 			document.documentElement.setAttribute(this.localStorageKey, value);
-			await new Promise(resolve => setTimeout(resolve, 1000));
-			document.documentElement.setAttribute(animationKey, before || "reduce");
+			document.documentElement.setAttribute(animationKey, before || "none");
 		});
 	}
 	//endregion
