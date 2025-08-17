dd645fd23ae6dff71f78aa660f4592707e2d19a7
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2025-08-18 02:51:27 +0300

    feat: add render function and update Vercel configuration for serverless deployment

--- api/render.js
+++ api/render.js
@@ -1,3 +1,4 @@
-import { handler } from '../dist/HomeSync/server/server.mjs';
-
-export default handler;
+export default async function handler(req, res) {
+	const {handler: angularHandler} = await import('../dist/HomeSync/server/server.mjs');
+	return angularHandler(req, res);
+}
