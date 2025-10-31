a85a42dfba5efa948b3ac3a29d2f06df12c28067
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2025-10-31 13:00:06 +0200

    refactor: streamline authentication handling and improve initialization logic

--- src/app/commons/interceptors/authorization.interceptor.ts
+++ src/app/commons/interceptors/authorization.interceptor.ts
@@ -4,12 +4,10 @@ import {AuthentificationService} from "@services/authentification.service";
 
 export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
 	const auth = inject(AuthentificationService);
-	if (auth.isAuthenticated()) {
-		req = req.clone({
-			setHeaders: {
-				Authorization: `Bearer ${auth.accessToken()}`
-			}
-		});
-	}
+	req = req.clone({
+		setHeaders: {
+			Authorization: `Bearer ${auth.accessToken()}`
+		}
+	});
 	return next(req);
 };
